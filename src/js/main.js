43570061c7004dd43c8b7514536a46639682db0b
anaischassot <Anais.Chassot@studentfr.ch>
2024-11-13 11:52:21 +0100

    scroll horizontal

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 // Puis le test de chargement du document
 document.addEventListener("DOMContentLoaded", function () {
-  // Enfin, tout le code JS ici
+  // scroll horizontal
+  // Crée un effet de scroll horizontal
+  gsap.to(".slider-container", {
+    x: "-80%", // chaque élément .slider-item vaut 20% de .slider-container, on veut donc bouger la position x de -80% pour faire apparaitre le dernier élément: chaque élément a besoin de 20% pour apparaitre pleinement sur l'écran.
+    scrollTrigger: {
+      trigger: ".slider-mask",
+      start: "top top",
+      end: "+=500%",
+      markers: true,
+      scrub: true,
+      pin: true,
+    },
+  });
 });
